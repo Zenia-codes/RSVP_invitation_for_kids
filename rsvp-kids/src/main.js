1b8a0342f81c4a9502db84f10bb307d22c340557
your GitHub login <zenia.codes@gmail.com>
2026-07-20 19:13:10 +0200

    style: added rainbow buttons

--- rsvp-kids/src/main.js
+++ rsvp-kids/src/main.js
@@ -1,5 +1,6 @@
 import "./style.scss";
 import "./components/form/form_gifts.js";
 import "./components/form/form_validation.js";
+import "./components/translate/language.js";
 
 // console.log("RSVP app started");
