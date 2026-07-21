5fa9b61ba8bef8848b1c873b31e898e4c7e756b1
your GitHub login <zenia.codes@gmail.com>
2026-07-21 15:24:19 +0200

    feat: validation of form, translate JS messages, style inv.

--- rsvp-kids/src/components/form/form_validation.js
+++ rsvp-kids/src/components/form/form_validation.js
@@ -1,8 +1,10 @@
+import { t, changeLanguage } from "../translate/language.js";
+
 const form = document.querySelector(".form");
 const errorMessage = document.querySelector("#errorMessage");
 const successMessage = document.querySelector("#successMessage");
 
-form.addEventListener("submit", function (e) {
+form.addEventListener("submit", async function (e) {
   e.preventDefault();
 
   //nejprve reset stareho stavu f.
@@ -19,69 +21,90 @@ form.addEventListener("submit", function (e) {
     'input[name="attendance"]:checked'
   );
   const adults = Number(document.querySelector("#numberOfAdults").value);
+  const [emailName, emailDomain] = email.split("@");
 
   if (name.length <= 2) {
-    errorMessage.textContent = "Zadej prosím celé jméno 🙂";
+    errorMessage.dataset.i18n = "messages.validation.nameTooShort";
+    errorMessage.textContent = t("messages.validation.nameTooShort");
     errorMessage.classList.remove("hidden");
     return;
   }
 
   if (name.length > 25) {
-    errorMessage.textContent = "Jméno je nějak podezřele dlouhé 😄";
+    errorMessage.dataset.i18n = "messages.validation.nameTooLong";
+    errorMessage.textContent = t("messages.validation.nameTooLong");
     errorMessage.classList.remove("hidden");
     return;
   }
 
   if (!/^[A-Za-zÁ-ž\s'-]+$/.test(name)) {
-    errorMessage.textContent = "Jméno obsahuje nepovolené znaky 🤔";
+    errorMessage.dataset.i18n = "messages.validation.nameIsWrong";
+    errorMessage.textContent = t("messages.validation.nameIsWrong");
     errorMessage.classList.remove("hidden");
     return;
   }
 
   if (surname.length <= 2) {
-    errorMessage.textContent = "Zadej prosím celé příjmení 🙂";
+    errorMessage.dataset.i18n = "messages.validation.shortSurname";
+    errorMessage.textContent = t("messages.validation.shortSurname");
     errorMessage.classList.remove("hidden");
     return;
   }
 
   if (surname.length > 25) {
-    errorMessage.textContent = "Příjmení je nějak podezřele dlouhé 😄";
+    errorMessage.dataset.i18n = "messages.validation.surnameTooLong";
+    errorMessage.textContent = t("messages.validation.surnameTooLong");
     errorMessage.classList.remove("hidden");
     return;
   }
 
   if (!/^[A-Za-zÁ-ž\s'-]+$/.test(surname)) {
-    errorMessage.textContent = "Příjmení obsahuje nepovolené znaky 🤔";
+    errorMessage.dataset.i18n = "messages.validation.surnameIsWrong";
+    errorMessage.textContent = t("messages.validation.surnameIsWrong");
     errorMessage.classList.remove("hidden");
     return;
   }
 
-  if (!email.includes("@")) {
-    errorMessage.textContent = "E-mailová adresa nevypadá správně 🤔";
+  if (!email.includes("@") || email.startsWith("@") || email.endsWith("@")) {
+    errorMessage.dataset.i18n = "messages.validation.emailInvalid";
+    errorMessage.textContent = t("messages.validation.emailInvalid");
     errorMessage.classList.remove("hidden");
     return;
   }
 
-  if (!agree) {
-    errorMessage.textContent =
-      "Pro pokračování je potřeba souhlasit s podmínkami 🙏";
+  if (!emailDomain.includes(".")) {
+    errorMessage.dataset.i18n = "messages.validation.emailMissingDot";
+    errorMessage.textContent = t("messages.validation.emailMissingDot");
     errorMessage.classList.remove("hidden");
     return;
   }
 
   if (!selectedRadio) {
-    errorMessage.textContent = "Dej nám prosím vědět, jestli dorazíš ✨";
+    errorMessage.dataset.i18n = "messages.validation.attendance";
+    errorMessage.textContent = t("messages.validation.attendance");
     errorMessage.classList.remove("hidden");
     return;
   }
 
   if (adults === 0) {
-    errorMessage.textContent = "Musí být alespoň 1 dospělý😊";
+    errorMessage.dataset.i18n = "messages.validation.adults";
+    errorMessage.textContent = t("messages.validation.adults");
     errorMessage.classList.remove("hidden");
     return;
   }
 
-  successMessage.textContent =
-    "Děkujeme ✨ Formulář byl úspěšně odeslán. Kopii odpovědí najdeš ve své e-mailové schránce 💌";
+  if (!agree) {
+    errorMessage.dataset.i18n = "messages.validation.consent";
+    errorMessage.textContent = t("messages.validation.consent");
+    errorMessage.classList.remove("hidden");
+    return;
+  }
+
+  successMessage.dataset.i18n = "messages.submit.success";
+  successMessage.textContent = t("messages.submit.success");
   successMessage.classList.remove("hidden");
+
+  // errorMessage.dataset.i18n = "messages.submit.error";
+  // errorMessage.textContent = t("messages.submit.error");
+  // errorMessage.classList.remove("hidden");
 });
